ecf98fc2dfc82e819bb1ffc25089c74507a2468b
miriyungreis4121 <miriyungreis@gmail.com>
2020-01-20 15:58:56 +0200

    adding clientHandler interface, MySerialServer and MyTestClientHandler classes

---
+++ ClientHandler.h
@@ -0,0 +1,12 @@
+//
+// Created by miri on 18.1.2020.
+//
+
+#ifndef EX4__CLIENTHANDLER_H_
+#define EX4__CLIENTHANDLER_H_
+#include <iostream>
+class ClientHandler {
+ public:
+  virtual void HandleClient(int i, int o);
+};
+#endif //EX4__CLIENTHANDLER_H_
