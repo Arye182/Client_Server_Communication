3b724210d2ef7cba52f7dfda43ed3d5e3d97f4d2
miriyungreis4121 <miriyungreis@gmail.com>
2020-01-23 19:34:09 +0200

    update cache data and MySerialServer.cpp

--- MySerialServer.cpp
+++ MySerialServer.cpp
@@ -15,10 +15,10 @@ void MySerialServer::open(int port, ClientHandler *c) {
     exit(1);
   }
 //define timeout for connecting client
-  int timeout_in_seconds = 12000;
-  struct timeval tv;
-  tv.tv_sec = timeout_in_seconds;
-  setsockopt(socketfd, SOL_SOCKET, SO_RCVTIMEO, (const char *) &tv, sizeof(tv));
+//  int timeout_in_seconds = 12000;
+//  struct timeval tv;
+//  tv.tv_sec = timeout_in_seconds;
+//  setsockopt(socketfd, SOL_SOCKET, SO_RCVTIMEO, (const char *) &tv, sizeof(tv));
 //bind socket to port
   bzero((char *) &serv_address, sizeof(serv_address));
   serv_address.sin_family = AF_INET;
@@ -46,11 +46,15 @@ void MySerialServer::stop(int socket) {
 void *MySerialServer::start(int *socket, ClientHandler *c) {
   struct sockaddr_in client_adress{};
   while (1) {
+    int timeout_in_seconds = 60;
+    struct timeval tv;
+    tv.tv_sec = timeout_in_seconds;
+    setsockopt(*socket, SOL_SOCKET, SO_RCVTIMEO, (const char *) &tv, sizeof(tv));
     int client_length = sizeof(client_adress);
     //accept client
     int client_socket = accept(*socket, (struct sockaddr *) &client_adress, (socklen_t *) &client_length);
     if (client_socket < 0) {
-      cerr << "eror accepting client" << endl;
+      cout << "no client found\nDone\n" << endl;
       stop(*socket);
       return nullptr;
     }
